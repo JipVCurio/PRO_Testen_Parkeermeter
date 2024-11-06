158c8191d6419d3168d053289f34c92a60603fda
JipVCurio <jv49@curio.nl>
2024-11-06 17:24:48 +0100

    Add files via upload

---
+++ ParkingMeter.cs
@@ -0,0 +1,57 @@
+﻿class ParkingMeter
+{
+    private decimal money = 0;
+    private const int maxMinutes = 120;
+    private const decimal pricePerMinute = 0.1M;
+
+    //Returns nothing, just adds money (largest acceptable banknote is a '100')
+    public void addMoney(int euros)
+    {
+        if (euros < 100)
+        {
+            money += euros;
+        }
+    }
+
+    //Returns current paid-for time (decimal)
+    public decimal getTime()
+    {
+        decimal time = money / pricePerMinute;
+        if (time < maxMinutes)
+        {
+            return time;
+        }
+        else
+        {
+            return maxMinutes;
+        }
+    }
+
+    //Returns leftover money (decimal)
+    public decimal buySlip()
+    {
+        decimal moneySpent = getTime() * pricePerMinute;
+        decimal moneyLeft = money - moneySpent;
+        return moneyLeft;
+    }
+
+    //Returns maximum parking time (int)
+    public int getMaxTime()
+    {
+        return maxMinutes;
+    }
+
+    //Return price per minute (decimal)
+    public decimal getPricePerMinute()
+    {
+        return pricePerMinute;
+    }
+
+    //Returns current balance (decimal)
+    public decimal getMoney()
+    {
+        return money;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        ParkingMeter meter = new ParkingMeter();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Maximale parkeertijd: " + meter.getMaxTime() + "mins");
+            Console.WriteLine("Prijs per minuut: €" + meter.getPricePerMinute());
+            Console.WriteLine();
+            Console.WriteLine("Bedrag toegevoegd: €" + meter.getMoney());
+            Console.WriteLine("Gekochte parkeertijd: " + meter.getTime() + "mins");
+            Console.WriteLine();
+            Console.WriteLine("Geld toevoegen, of typ 'koop':");
+            string input = Console.ReadLine();
+            if (input == "koop")
+            {
+                decimal moneyLeft = meter.buySlip();
+                Console.Clear();
+                Console.WriteLine("Ticket gekocht voor " + meter.getTime() + " minuten");
+                Console.WriteLine("Geld retour: " + moneyLeft);
+                break;
+            }
+
+            int money = Convert.ToInt32(input);
+            meter.addMoney(money);
+        }
+    }
+}
